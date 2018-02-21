be291c7937ccdc2b13c2fb6e1c57f32604a9be79
alberto56 <albert@mediatribe.net>
2018-02-20 19:39:19 -0500

    Added a report on the current space station location

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-Consolidator
+Consolidator for Drupal 7
 =====
+
+**This is a proof of concept and not yet meant for production use**.
 
 Consolidate information from a number of sources, for example REST APIs or the Drupal database, into a simple report.
 
 Comes with an example report, but requires development to create custom reports based on the example provided.
 
-Development
+Usage
+-----
+
+Install consolidator_example, go to /admin/reports/consolidator, and select the report "Star wars characters average height vs. average air quality measurements in cities". Submit and view the report results.
+
+Extending this module
+-----
+
+Build your own reports by copy-pasting the consolidator_example module and creating your own module based on that code. Clear cache, and your reports will appear in /admin/reports/consolidator.
+
+Development and demos
 -----
 
 Install Docker and run:
@@ -17,6 +29,14 @@ This will give you a login link to a development environment.
 To run a drush command, run:
 
     docker-compose exec web /bin/bash -c 'drush help'
+
+To temporarily shut down your development environment, run:
+
+    docker-compose down
+
+Bring it back up by typing
+
+    ./scripts/deploy.sh
 
 To destroy your development environment, run:
 
---
+++ consolidator_example/src/ReportType/CurrentSpaceStationLocation.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace consolidator_example\ReportType;
+
+use consolidator\ReportType\ReportType;
+use consolidator\Report\Report;
+
+/**
+ * A sample report to copy-paste and build your own reports.
+ */
+class CurrentSpaceStationLocation extends ReportType {
+
+  /**
+   * Display markup for a report.
+   *
+   * @param array $report
+   *   A report as generated from ::buildReport().
+   *
+   * @return string
+   *   Markup.
+   */
+  public function displayReport(array $report) : string {
+    return '
+    <p>Latitude: ' . $report['lat'] . ', longitude: ' . $report['lon'] . '</p><script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script>
+function initialize() {
+var map_canvas = document.getElementById("map_canvas");
+var map_options = {
+center: new google.maps.LatLng(' . $report['lat'] . ', ' . $report['lon'] . '),
+zoom:5,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+var map = new google.maps.Map(map_canvas, map_options)
+var marker = new google.maps.Marker({
+  position: {lat: ' . $report['lat']  . ', lng: ' . $report['lon'] . '},
+  map: map,
+  title: "Position of the international space station"
+});
+}
+google.maps.event.addDomListener(window, "load", initialize);
+</script>
+</head>
+<body>
+<div id="map_canvas" style="height:500px; width: 500px;"></div>';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function name() : string {
+    return 'Current location of the international space station';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function steps() : array {
+    return [
+      'getSpaceStationInfo' => [],
+    ];
+  }
+
+  /**
+   * Step 1, get Space station info.
+   */
+  public function getSpaceStationInfo() {
+    return $this->getJson('http://api.open-notify.org/iss-now.json');
+  }
+
+  /**
+   * Final step, build a report.
+   */
+  public function buildReport() : array {
+    $info = $this->get('getSpaceStationInfo');
+
+    return [
+      'lat' => $info['iss_position']['latitude'],
+      'lon' => $info['iss_position']['longitude'],
+    ];
+  }
+
+}
